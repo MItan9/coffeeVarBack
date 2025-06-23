fdf1c7f1535d6a105e31cab0d266bfe05dc969ea
Mihailova Tatiana <tatiana.mihailova@isa.utm.md>
2025-06-23 19:50:56 +0300

    qr code generation

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const authRoutes = require("./routes/auth");
 require("dotenv").config();
+const qrRoutes = require("./routes/qrcode");
+const passport = require("passport");
+const googleAuth = require("./routes/google"); // Инициализация Google OAuth
+const session = require("express-session");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use("/auth", authRoutes);
+app.use("/", qrRoutes);
+
+app.use(session({ secret: "coffee_secret", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(googleAuth);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
